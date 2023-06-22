fb95bc0f3219a1615eebdb2afe773372d29e4641
iliampav <iliamfer@hotmail.com>
2023-06-21 22:43:02 -0300

    finish section one and started section two

---
+++ src/components/formsTitles/index.tsx
@@ -0,0 +1,48 @@
+import styles from './styles.module.scss';
+
+import OrangeForm from '@/public/assets/svgs/firstForm.svg';
+import GreenForm from '@/public/assets/svgs/secondForm.svg';
+import BlueForm from '@/public/assets/svgs/thirdForm.svg';
+
+interface formsTitleInterface {
+    firstTitle: string, 
+    firstSubTitle: string, 
+    secondTitle: string, 
+    secondSubTitle: string, 
+    thirdTitle: string, 
+    thirdSubTitle: string
+}
+
+export default function FormsTopics(props: formsTitleInterface) {
+
+    const {firstTitle, firstSubTitle, secondTitle, secondSubTitle, thirdTitle, thirdSubTitle} = props
+
+    return (
+        <section className={styles.formsTopicsSection}>
+            <div className={`container ${styles.formsTopics}`}>
+                <div className={styles.singleFormBox}>
+                    <OrangeForm />
+                    <div>
+                        <h3>{firstTitle}</h3>
+                        <p>{firstSubTitle}</p>
+                    </div>
+                </div>
+                <div className={`${styles.singleFormBox} ${styles.center}`}>
+                    <GreenForm />
+                    <div>
+                        <h3>{secondTitle}</h3>
+                        <p>{secondSubTitle}</p>
+                    </div>
+                </div>
+                <div className={`${styles.singleFormBox} ${styles.flexEnd}`}>
+                    <BlueForm />
+                    <div>
+                        <h3>{thirdTitle}</h3>
+                        <p>{thirdSubTitle}</p>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.backgroundPlay}></div>
+        </section>
+    )
+}
