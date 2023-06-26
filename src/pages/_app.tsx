3a56b000ef5ae48b222af4dcf87273ff665b55dd
iliampav <iliamfer@hotmail.com>
2023-06-26 11:33:56 -0300

    add webreportvials

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import type { AppProps } from 'next/app'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
 import '../styles/globals.scss'
 import "swiper/css";
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  if (metric.label === 'web-vital') {
+    console.log(metric) // The metric object ({ id, name, startTime, value, label }) is logged to the console
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
