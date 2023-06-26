bb90b0d5d969f141a11982c30c8fcccfd8fb4e87
iliampav <iliamfer@hotmail.com>
2023-06-26 11:30:00 -0300

    improved fonts import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,7 @@ export default class MyDocument extends Document {
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com/" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
-                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700&family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" /> 
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@;400;500;600;700&family=Poppins:wght@400;600;700" rel="stylesheet" /> 
                     <link rel="icon" href="dice.ico" type="image/x-icon"/>
                 </Head>
                 <body>
