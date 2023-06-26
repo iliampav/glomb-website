9f44774b88ce95343192c9986bd73af369819222
iliampav <iliamfer@hotmail.com>
2023-06-26 11:41:51 -0300

    add test for scripting faster

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import Script from 'next/script'
+
 import '../styles/globals.scss'
 import "swiper/css";
 
@@ -9,7 +11,12 @@ export function reportWebVitals(metric: NextWebVitalsMetric) {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Component {...pageProps} />
+        <Script src="https://fonts.googleapis.com/css2?family=Inter:wght@;400;500;600;700&family=Poppins:wght@400;600;700" rel="stylesheet" />
+      </>
+  )
 }
 
 export default MyApp
