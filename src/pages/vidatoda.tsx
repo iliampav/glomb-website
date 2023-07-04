350f3b4d547640b00a70b5f8ade6e01d087bb19d
iliampav <iliamfer@hotmail.com>
2023-07-04 08:53:56 -0300

    change on image

--- src/pages/vidatoda.tsx
+++ src/pages/vidatoda.tsx
@@ -19,11 +19,8 @@ return (
       {/* Image shared in web social links */}
       <meta property='og:title' content='O valor da sua aposentadoria pode dobrar'/>
       <meta property='og:image' content={'/assets/images/aposentadoria/cover.jpg'}/>
-      <meta property="og:image:type" content="image/jpg" />
       <meta property='og:description' content='Descubra como aumentar o valor da sua aposentadoria e receber uma indenização.'/>
       <meta property='og:url' content='https://glomb-website.vercel.app/vidatoda/'/>
-      <meta property='og:image:width' content='1200'/>
-      <meta property='og:image:height' content='627'/>
 
       <title>Glomb - O valor da sua aposentadoria pode dobrar</title>
       <meta name="keywords" content="aposentadoria, aposentado, pensionista, revisão, vida toda, INSS, Advogado"></meta>
