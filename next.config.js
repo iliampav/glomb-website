befa75f47f11b820a2a321848b34b7ac069e2dc0
iliampav <iliamfer@hotmail.com>
2023-07-04 10:55:28 -0300

    change on cache

--- next.config.js
+++ next.config.js
@@ -12,6 +12,20 @@ const nextConfig = {
   
       return config
     },
+    async headers() {
+      return [
+        {
+          source: '/:all*(svg|jpg|png)',
+          locale: false,
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'public, max-age=9999999999, must-revalidate',
+            }
+          ],
+        },
+      ]
+    },
   }
   
   module.exports = nextConfig
