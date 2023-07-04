6156fe87f40b643961557abb81b0fc7d95da1b4b
iliampav <iliamfer@hotmail.com>
2023-07-03 22:46:59 -0300

    test whats

--- src/pages/vidatoda.tsx
+++ src/pages/vidatoda.tsx
@@ -18,7 +18,8 @@ return (
 
       {/* Image shared in web social links */}
       <meta property='og:title' content='O valor da sua aposentadoria pode dobrar'/>
-      <meta property='og:image' content='/assets/images/aposentadoria/cover.jpg'/>
+      <meta property='og:image' content='assets/images/aposentadoria/cover.jpg'/>
+      <meta property="og:image:type" content="image/jpg" />
       <meta property='og:description' content='Descubra como aumentar o valor da sua aposentadoria e receber uma indenização.'/>
       <meta property='og:url' content='https://www.previdenciario.glomb.com.br/vidatoda'/>
       <meta property='og:image:width' content='1200'/>
